70b3c0eb217644080fc144f0cd046a7225b52ae5
xandraco <andraco7x7@outlook.com>
2024-05-14 16:50:32 -0600

    finished the header structure

--- src/app/components/ui-header/ui-header.component.ts
+++ src/app/components/ui-header/ui-header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-ui-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridListModule
+  ],
   templateUrl: './ui-header.component.html',
   styleUrl: './ui-header.component.css'
 })
